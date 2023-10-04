3bd3656cc84b6f586d847c3cd3ad59c702e72b10
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2023-10-04 10:46:42 +0545

    authenticate routing and login

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
 import mongoose from "mongoose";
+import router from "./router";
+
 require("dotenv").config();
 
 const app = express();
@@ -24,5 +26,6 @@ const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
-
 mongoose.connection.on("error", (error: Error) => console.log(error));
+
+app.use("/", router());
